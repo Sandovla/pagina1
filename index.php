a7f54a1d985fe32ed56162a5b9464b3a6bcfe704
Sandovla <152697420+Sandovla@users.noreply.github.com>
2023-12-02 06:48:48 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<html>
+
+<head>          
+    <title>Inicio</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/estilos.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/card.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/tabla.css" rel="stylesheet" type="text/css" />
+</head>
+<style>
+    .foto {
+        height: 260px;
+        width: 270px;
+        border-radius: 50%;
+        background-size: 100% auto;
+
+    }
+
+    .izquierda {
+        text-align: left;
+    }
+
+    .tabla-body {
+        width: 600px;
+        margin: auto;
+    }
+
+</style>
+
+<body>
+    <div class="container">
+        <main class="cd_main">
+            <div class="grid">
+                <div class="grid_item">
+                    <div class="centrado">
+                        <br />
+                        <img src="assets/img/foto.jpg" class="foto" alt="mi foto">
+                        <br /><br />
+                        <a href="vista/inicio.php" class="card_btn" style="text-decoration: none;">Mi Pagina</a>
+                    </div>
+                    <br />
+                    <table class="table tabla-body" >
+                        <tbody>
+                            <tr style="border:hidden;">
+                                <td>
+                                    <strong>Proyecto:</strong>
+                                </td>
+                                <td class="izquierda">
+                                    <p>
+                                        Pagina web de regalos y detalles personalizados con vinil y papael "Finscraft"
+                                                          OXAPAMPA
+                                    </p>
+                                </td>
+                            </tr>
+
+                            <tr style="border:hidden;">
+                                <td>
+                                    <strong>Alumno:</strong>
+                                </td>
+                                <td class="izquierda">
+                                    <p>
+                                       Sandoval Herrera Josue Cristhian_ U20303026
+                                    </p>
+                                </td>
+                            </tr>
+
+                            <tr style="border:hidden;"> 
+                                <td>
+                                    <strong>Descripción:</strong>
+                                </td>
+                                <td class="izquierda">
+                                    <p>
+                                       En esta pagina web busco poder facilitar la interaccion del cliente con la empresa "Finscraft"
+                                       para que de esta manera el cliente pueda tener acceso a todos los productos y poder tener mas
+                                       conocimiento de la empresa en si, de igual manera busco que su experiencia en la pagina web sea muy 
+                                       interactiva y no sea una pagina aburrida, en este caso la empresa Finscraft damos vida a la imaginación a 
+                                       través de la magia del vinil y el papel. Somos tu destino para detalles únicos y personalizados que añaden
+                                       un toque especial a cualquier ocasión.
+                                    </p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
